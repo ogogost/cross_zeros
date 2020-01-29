1f69f478f50c278ea71e7d2c9ea7b94b9dc19e81
Galsan Dampilon <59030438+ogogost@users.noreply.github.com>
2020-01-29 10:23:42 +0300

    Update view.py

--- view.py
+++ view.py
@@ -1,3 +1,12 @@
+from cross_zeros.map import Field
+from cross_zeros.player import Player
 
-class View():
-    pass
+class View:
+    def __init__(self):
+        self.field = Field()
+
+    def display_field(self):
+        [print(self.field.data[i]) for i in range(len(self.field.data))]
+
+    # def display_players_status:
+    #     print("Player 1 :", self.player.type_player)
